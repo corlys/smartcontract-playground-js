2408be669f7dc7604ad0b35728bc2fd619d0df90
corlys <duscae047@gmail.com>
2022-06-16 13:01:03 +0700

    feat: change console log again

--- scripts/listen.js
+++ scripts/listen.js
@@ -4,46 +4,57 @@ const EventAbi = require("../artifacts/contracts/EventContract.sol/EventContract
 const EventAbiTwo = require("../artifacts/contracts/AnotherContract.sol/AnotherContract.json");
 
 async function main() {
-  const event1 = "0x99b8cC5B80a79d658513a9DF117320f4AE46Bb01";
-  const event2 = "0x0e460fAF94Cd5838b56CE555b55C328784e41305";
-  const provider = new ethers.providers.WebSocketProvider(
-    "wss://astar.blastapi.io/b47d84b0-7653-4de0-83ab-b71dbefe61f7",
-    { chainId: 592, name: "astar" }
-  );
-  const contract = new ethers.Contract(event1, EventAbi.abi, provider);
-  const contractTwo = new ethers.Contract(event2, EventAbiTwo.abi, provider);
+  try {
+    const event1 = "0x99b8cC5B80a79d658513a9DF117320f4AE46Bb01";
+    const event2 = "0x0e460fAF94Cd5838b56CE555b55C328784e41305";
+    const provider = new ethers.providers.WebSocketProvider(
+      "wss://astar.blastapi.io/b47d84b0-7653-4de0-83ab-b71dbefe61f7",
+      { chainId: 592, name: "astar" }
+    );
+    const contract = new ethers.Contract(event1, EventAbi.abi, provider);
+    const contractTwo = new ethers.Contract(event2, EventAbiTwo.abi, provider);
 
-  contract.on("TestEvent1", (sender, counter, timestamp) => {
-    console.log("Event TestEvent1");
-    console.log(`sender : ${sender}`);
-    console.log(`counter : ${counter}`);
-    console.log(`date : ${new Date(timestamp.toNumber() * 1000).toJSON()}\n`);
-  });
+    contract.on("TestEvent1", (sender, counter, timestamp) => {
+      console.log("Event TestEvent1");
+      console.log(`sender : ${sender}`);
+      console.log(`counter : ${counter}`);
+      console.log(`date : ${new Date(timestamp.toNumber() * 1000).toJSON()}\n`);
+    });
 
-  contract.on("TestEvent2", (sender, counter, timestamp) => {
-    console.log("Event TestEvent2");
-    console.log(`sender : ${sender}`);
-    console.log(`counter : ${counter}`);
-    console.log(`date : ${new Date(timestamp.toNumber() * 1000).toJSON()}\n`);
-  });
+    contract.on("TestEvent2", (sender, counter, timestamp) => {
+      console.log("Event TestEvent2");
+      console.log(`sender : ${sender}`);
+      console.log(`counter : ${counter}`);
+      console.log(`date : ${new Date(timestamp.toNumber() * 1000).toJSON()}\n`);
+    });
 
-  contractTwo.on("AnotherEvent1", (sender, counter, timestamp) => {
-    console.log("Event AnotherEvent1");
-    console.log(`sender : ${sender}`);
-    console.log(`counter : ${counter}`);
-    console.log(`date : ${new Date(timestamp.toNumber() * 1000).toJSON()}\n`);
-  });
+    contractTwo.on("AnotherEvent1", (sender, counter, timestamp) => {
+      console.log("Event AnotherEvent1");
+      console.log(`sender : ${sender}`);
+      console.log(`counter : ${counter}`);
+      console.log(`date : ${new Date(timestamp.toNumber() * 1000).toJSON()}\n`);
+    });
 
-  contractTwo.on("AnotherEvent2", (sender, counter, timestamp) => {
-    console.log("Event AnotherEvent2");
-    console.log(`sender : ${sender}`);
-    console.log(`counter : ${counter}`);
-    console.log(`date : ${new Date(timestamp.toNumber() * 1000).toJSON()}\n`);
-  });
+    contractTwo.on("AnotherEvent2", (sender, counter, timestamp) => {
+      console.log("Event AnotherEvent2");
+      console.log(`sender : ${sender}`);
+      console.log(`counter : ${counter}`);
+      console.log(`date : ${new Date(timestamp.toNumber() * 1000).toJSON()}\n`);
+    });
+  } catch (error) {
+    console.log(
+      "Somethings wrong inside of try catch, here's the message : ",
+      error
+    );
+    throw new Error(error);
+  }
 }
 
 main().catch((error) => {
-  console.log("Somethings wrong, here's the message : ", error);
+  console.log(
+    "Somethings wrong inside of main method, here's the message : ",
+    error
+  );
   // console.error(error);
   process.exitCode = 1;
 });
--- scripts/otomasi.js
+++ scripts/otomasi.js
@@ -8,35 +8,46 @@ const {
 const scheaduler = require("node-schedule");
 
 async function main() {
-  const [account1, account2] = await ethers.getSigners();
-  const address1 = "0x99b8cC5B80a79d658513a9DF117320f4AE46Bb01";
-  const address2 = "0x0e460fAF94Cd5838b56CE555b55C328784e41305";
-  const contract1 = new ethers.Contract(address1, abi1, account1);
-  scheaduler.scheduleJob("* * * * *", async () => {
-    const sendTx1 = await contract1.execEvent1();
-    await sendTx1.wait();
-    console.log("Exec Event 1 SC 1 hash: ", sendTx1.hash);
+  try {
+    const [account1, account2] = await ethers.getSigners();
+    const address1 = "0x99b8cC5B80a79d658513a9DF117320f4AE46Bb01";
+    const address2 = "0x0e460fAF94Cd5838b56CE555b55C328784e41305";
+    const contract1 = new ethers.Contract(address1, abi1, account1);
+    scheaduler.scheduleJob("* * * * *", async () => {
+      const sendTx1 = await contract1.execEvent1();
+      await sendTx1.wait();
+      console.log("Exec Event 1 SC 1 hash: ", sendTx1.hash);
 
-    const sendTx2 = await contract1.execEvent2();
-    await sendTx2.wait();
-    console.log("Exec Event 2 SC 1 hash: ", sendTx2.hash);
-  });
-  const contract2 = new ethers.Contract(address2, abi2, account2);
-  scheaduler.scheduleJob("* * * * *", async () => {
-    const sendTx1 = await contract2.connect(account2).execEvent1();
-    await sendTx1.wait();
-    console.log("Exec Event 1 SC 2 hash: ", sendTx1.hash);
+      const sendTx2 = await contract1.execEvent2();
+      await sendTx2.wait();
+      console.log("Exec Event 2 SC 1 hash: ", sendTx2.hash);
+    });
+    const contract2 = new ethers.Contract(address2, abi2, account2);
+    scheaduler.scheduleJob("* * * * *", async () => {
+      const sendTx1 = await contract2.connect(account2).execEvent1();
+      await sendTx1.wait();
+      console.log("Exec Event 1 SC 2 hash: ", sendTx1.hash);
 
-    const sendTx2 = await contract2.connect(account2).execEvent2();
-    await sendTx2.wait();
-    console.log("Exec Event 2 SC 2 hash: ", sendTx2.hash);
-  });
+      const sendTx2 = await contract2.connect(account2).execEvent2();
+      await sendTx2.wait();
+      console.log("Exec Event 2 SC 2 hash: ", sendTx2.hash);
+    });
+  } catch (error) {
+    console.log(
+      "Something is wrong in the trycatch block, heres the error ",
+      error
+    );
+    throw new Error(error);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.log("Somethings wrong, here's the message : ", error);
+  console.log(
+    "Somethings wrong in the main method, here's the message : ",
+    error
+  );
   // console.error(error);
   process.exitCode = 1;
 });
